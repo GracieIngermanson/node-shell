9dab582c5133011f36e4b4a2e8d623edabce81ae
Hatibe Kabulantok <hatibekara@gmail.com>
2020-11-13 22:59:53 +0300

    ls implemented

--- bash.js
+++ bash.js
@@ -1,11 +1,17 @@
+const pwd = require('./pwd');
+//const { lstat } = require('fs');
+const ls = require('./ls');
+
 process.stdout.write ('prompt > ');
 process.stdin.on('data',  (data) => {
-  const cmd = data.toString().trim();
+  let cmd = data.toString().trim();
   if (cmd === 'pwd') {
-    process.stdout.write(__dirname);
+    pwd();
+  } else if (cmd === 'ls') {
+    ls();
   } else {
     process.stdout.write('You typed ' + cmd);
+    process.stdout.write('\nprompt > ');
   }
-
-  process.stdout.write('\nprompt > ');
 });
+
